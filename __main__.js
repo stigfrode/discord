553ded6c5af5467bc640e1390ccf7bf8ccc1b1fd
stigfrode <95746375+stigfrode@users.noreply.github.com>
2022-11-17 15:40:06 +0100

    Add files via upload

---
+++ __main__.js
@@ -0,0 +1,20 @@
+{
+  ('name');
+  'Synology NAS', 'type';
+  1, 'channel_id';
+  '1040261124924379146', 'token';
+  'MTA0MDI1NjU0MjA2NDI0Njg1NA.Gz0CZv.ninYRwmsWEDglCqCkW2vSyIRZOb22JbcBT8Rv8',
+    'avatar';
+  null, 'guild_id';
+  '835234121621307482', 'id';
+  '1040256542064246854', 'application_id';
+  null, 'user';
+  {
+    ('username');
+    'Synology NAS', 'discriminator';
+    '1707', 'id';
+    '1040256542064246854', 'avatar';
+    'b33065c0e62095c9cd6ff41622e4294c', 'public_flags';
+    131328;
+  }
+}
